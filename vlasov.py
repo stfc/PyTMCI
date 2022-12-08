f1936db7edad777d824291236575c83de395ec0c
David Posthuma de Boer <david.posthuma-de-boer@stfc.ac.uk>
2022-12-08 16:07:03 +0000

    Style and Linting. No real changes.

--- vlasov.py
+++ vlasov.py
@@ -1,14 +1,14 @@
-import multiprocessing as mp
-import numpy as np                   # Linear algebra and mathematical functions
-import scipy.constants as cn         # Physical constants
-import scipy.integrate as si         # Numerical integration methods (Romberg) 
+import numpy as np               # Linear algebra and mathematical functions
+import scipy.constants as cn     # Physical constants
 import matplotlib.pyplot as plt
 import laguerre_methods as lm
 import airbag_methods as am
 import nht_methods as nht
 
+
 class accelerator:
-    def __init__(self, circumference, mass, charge, beta, Q_b, Q_s, alpha, chromaticity, num_bunches):
+    def __init__(self, circumference, mass, charge, beta, Q_b, Q_s, alpha,
+                 chromaticity, num_bunches):
         self.circumference = circumference
         self.mass = mass
         self.charge = charge
@@ -19,16 +19,16 @@ class accelerator:
         self.chromaticity = chromaticity
         self.num_bunches = num_bunches
 
-        self.gamma = 1/np.sqrt(1-beta**2)
-        self.f0 = 1/(circumference/beta/cn.c)
-        self.T0 = 1/self.f0
-        self.w0 = 2*np.pi*self.f0
-        self.w_b = 2*np.pi * Q_b * self.f0
-        self.w_s = 2*np.pi * Q_s * self.f0
+        self.gamma = 1 / np.sqrt(1 - beta**2)
+        self.f0 = 1 / (circumference / beta / cn.c)
+        self.T0 = 1 / self.f0
+        self.w0 = 2 * np.pi * self.f0
+        self.w_b = 2 * np.pi * Q_b * self.f0
+        self.w_s = 2 * np.pi * Q_s * self.f0
 
-        self.eta = alpha - 1/self.gamma**2
+        self.eta = alpha - 1 / self.gamma**2
         self.w_xi = self.w_b * self.chromaticity / self.eta
-    
+
 
 class vlasovSolver():
     def __init__(self, accelerator, max_l, max_n, *args, **kwargs):
@@ -36,15 +36,15 @@ class vlasovSolver():
         self.max_n = max_n
         self.accelerator = accelerator
 
-        self.l_matrix = np.zeros(shape=((2*max_l+1)*(max_n+1), 
-                                           (2*max_l+1)*(max_n+1)),
-                                    dtype=np.complex128)
+        self.l_matrix = np.zeros(shape=((2 * max_l + 1) * (max_n + 1),
+                                        (2 * max_l + 1) * (max_n + 1)),
+                                 dtype=np.complex128)
         self.l_matrix = self.generateLMatrix()
 
-        self.base_matrix = np.zeros(shape=((2*max_l+1)*(max_n+1), 
-                                           (2*max_l+1)*(max_n+1)),
+        self.base_matrix = np.zeros(shape=((2 * max_l + 1) * (max_n + 1),
+                                           (2 * max_l + 1) * (max_n + 1)),
                                     dtype=np.complex128)
-    
+
     def impedanceSampleFrequencies(self, max_harmonics, multi_bunch_mode):
         '''
         Returns the quantities
@@ -62,13 +62,14 @@ class vlasovSolver():
                              + "the number of bunches.")
 
         p = np.arange(-max_harmonics, max_harmonics + 1, dtype=np.float64)
-        fp = (p * self.accelerator.num_bunches + multi_bunch_mode) * self.accelerator.f0 + self.accelerator.w_b / 2 / np.pi
+        fp = ((p * self.accelerator.num_bunches + multi_bunch_mode) * self.accelerator.f0
+              + self.accelerator.w_b / 2 / np.pi)
         # note we're skipping the l*omega_s
         return fp
 
     def calculateGk(self):
         print("Only for arbitraryLongitudinal distributions.")
-    
+
     def g0Hat(self):
         pass
 
@@ -85,10 +86,15 @@ class vlasovSolver():
     def generateInteractionMatrix(self, N):
         assert self.base_matrix[0, 0] != 0.0, "generateBaseMatrix() must be run before generateInteractionMatrix()." 
 
-        interactionMatrix = lm.generateInteractionMatrix(
-            self.base_matrix, self.l_matrix,
-            N, self.accelerator.num_bunches, self.accelerator.mass, self.accelerator.beta,
-            self.accelerator.f0, self.accelerator.w_b, self.accelerator.w_s)
+        interactionMatrix = lm.generateInteractionMatrix(self.base_matrix,
+                                                         self.l_matrix,
+                                                         N,
+                                                         self.accelerator.num_bunches,
+                                                         self.accelerator.mass,
+                                                         self.accelerator.beta,
+                                                         self.accelerator.f0,
+                                                         self.accelerator.w_b,
+                                                         self.accelerator.w_s)
 
         return interactionMatrix
 
@@ -96,38 +102,36 @@ class vlasovSolver():
         '''
         '''
         print("Not Implemented for this distribution.")
-    
+
     def Omega(self, eigenvalue):
         return eigenvalue * self.accelerator.w_s + self.accelerator.w_b
-
-        
 
 
 class airbag(vlasovSolver):
     def __init__(self, accelerator, max_l, zhat, *args, **kwargs):
         super().__init__(accelerator, max_l, 0)
         self.zhat = zhat
-    
+
     def generateBaseMatrix(self, sample_frequencies, sampled_dipole_impedance):
         self.base_matrix = am.generateBaseMatrix(
             self.max_l, self.zhat,
             sample_frequencies, sampled_dipole_impedance,
             self.accelerator.f0, self.accelerator.num_bunches,
             self.accelerator.beta, self.accelerator.w_b, self.accelerator.w_xi)
-        
-        return self.base_matrix
 
+        return self.base_matrix
 
 
 class NHT(vlasovSolver):
     def __init__(self, accelerator, max_l, num_rings, *args, **kwards):
-        super().__init__(accelerator, max_l, num_rings-1)
+        super().__init__(accelerator, max_l, num_rings - 1)
 
         self.ring_radii = np.zeros([])
         self.num_rings = num_rings
-        
+
     def calculateRingRadii(self, g0hat, search_limit):
-        self.ring_radii = nht.calculateRingRadii(g0hat, self.num_rings, search_limit)
+        self.ring_radii = nht.calculateRingRadii(g0hat, self.num_rings,
+                                                 search_limit)
         return self.ring_radii
 
     def generateBaseMatrix(self, sample_frequencies, sampled_dipole_impedance):
@@ -140,9 +144,8 @@ class NHT(vlasovSolver):
             sample_frequencies, sampled_dipole_impedance,
             self.accelerator.f0, self.accelerator.num_bunches,
             self.accelerator.beta, self.accelerator.w_b, self.accelerator.w_xi)
-        
-        return self.base_matrix
 
+        return self.base_matrix
 
 
 class arbitraryLongitudinal(vlasovSolver):
@@ -151,8 +154,9 @@ class arbitraryLongitudinal(vlasovSolver):
 
         self.a = 0
         self.Gk = np.array([])
-    
-    def calculateGk(self, g0hat, a, terms, max_radius=1, numerical_normalise=False, *args, **kwargs):
+
+    def calculateGk(self, g0hat, a, terms, max_radius=1,
+                    numerical_normalise=False, *args, **kwargs):
         Gk = np.zeros(terms, dtype=np.float64)
         for k in range(terms):
             Gk[k] = lm.calculateGk(g0hat, a, k, **kwargs)
@@ -161,14 +165,15 @@ class arbitraryLongitudinal(vlasovSolver):
         self.Gk = Gk
 
         return (a, self.Gk)
-    
+
     def g0Hat(self, r):
         return lm.g0Hat(r, self.a, self.Gk)
 
     def generateBaseMatrix(self, sample_frequencies, sampled_dipole_impedance):
         if len(self.Gk) == 0:
-            raise Exception("Unperturbed longitudinal distribution has not been fit. \n " +\
-            "Run calculateGk() before calculating a base matrix.")
+            raise Exception("Unperturbed longitudinal distribution has not"
+                            + "been fit. \n Run calculateGk() before"
+                            + "calculating a base matrix.")
             return self.base_matrix
 
         self.base_matrix = lm.generateBaseMatrix(
@@ -176,23 +181,24 @@ class arbitraryLongitudinal(vlasovSolver):
             sample_frequencies, sampled_dipole_impedance,
             self.accelerator.f0, self.accelerator.num_bunches,
             self.accelerator.beta, self.accelerator.w_b, self.accelerator.w_xi)
-        
+
         return self.base_matrix
-    
+
     def g1(self, eigenvectors, max_r, grid_size):
         return lm.g1(grid_size, self.a, eigenvectors, max_r,
-        self.max_l, self.max_n,
-        self.accelerator.beta, self.accelerator.chromaticity,
-        self.accelerator.w_b, self.accelerator.eta)
+                     self.max_l, self.max_n,
+                     self.accelerator.beta, self.accelerator.chromaticity,
+                     self.accelerator.w_b, self.accelerator.eta)
 
-## Helper Functions
+
+# Helper Functions
 def find_most_unstable(eigenvalues):
     '''
     Find the mode unstable mode
-    
+
     The mode the the largest positive imaginary part is the
     mode with the fastest growth rate. This can be of particular
-    interest if one mode dominates since it can then predict 
+    interest if one mode dominates since it can then predict
     growth times and the expected distribution.
     '''
     eigenvalue_index = np.argmax(np.imag(eigenvalues))
