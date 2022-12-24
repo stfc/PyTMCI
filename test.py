b03f500af45c9b2e215ba99cf9a955adba56f0b8
David Posthuma de Boer <david.posthuma-de-boer@stfc.ac.uk>
2022-12-24 13:20:55 +0000

    TEST: Add test for NHT perturbed-simple
    Matrix is computed with a different loop structure to the ordinary
    function. No symmetry is utilised.

--- test.py
+++ test.py
@@ -252,5 +252,92 @@ class TestAirbag(unittest.TestCase):
                         self.assertIsNone(np.testing.assert_array_almost_equal(base_matrix, my_base_matrix))
 
 
+class TestNHT(unittest.TestCase):
+    def test_generateSimpleBaseMatrix(self):
+        import scipy.special as sp
+
+        circumference = 523.456
+        mass = 1.5e-20
+        charge = 1.6e-19
+        beta = 0.5
+        Qb = 3.9
+        Qs = 0.34
+        alpha = 0.22
+        chromaticity = -1.5
+        num_bunches = 2
+
+        acc1 = vs.accelerator(circumference,
+                              mass,
+                              charge,
+                              beta,
+                              Qb,
+                              Qs,
+                              alpha,
+                              chromaticity,
+                              num_bunches)
+        num_rings = 2
+        max_l = 3
+        max_n = num_rings - 1
+        ring_radii = np.array([1.5, 3.0])  # Just set some constants
+
+        # Calculate impedance sample frequencies
+        max_harmonics = 5
+        multi_bunch_mode = 0
+
+        # Calculate slippage factor.
+        f0 = beta * cn.c / circumference
+        w_b = 2 * np.pi * Qb * f0
+        gamma = 1 / np.sqrt(1 - beta**2)
+        eta = alpha - 1 / gamma**2  # Chao Eq. 1.10, pp. 9
+        w_xi = chromaticity * w_b / eta  # Chao Eq. 6.181, pp. 338
+
+        # Verify that accelerator parameters have been worked out correctly
+        self.assertEqual(f0, acc1.f0)
+        self.assertEqual(w_b, acc1.w_b)
+        self.assertEqual(gamma, acc1.gamma)
+        self.assertEqual(eta, acc1.eta)
+        self.assertEqual(w_xi, acc1.w_xi)
+
+        p = np.arange(-max_harmonics, max_harmonics + 1, dtype=np.float64)
+        # Often impedance is given as a function of f=w/2/pi, rather than w so
+        # find this first
+        fp = ((p * num_bunches + multi_bunch_mode) * f0 + Qb * f0)
+        wp = 2 * np.pi * fp
+        w = wp - w_xi
+
+        # for perturbed-simple w is a 1D array. All bessel functions and
+        # impedances are sampled at the same frequencies.
+        orders = list(range(0, max_l + 1)) + list(range(-max_l, 0))
+        jdict = [[sp.jn(i, w * zhat / beta / cn.c) for i in orders] for zhat in ring_radii]
+
+        def zperp(f):
+            return np.cos(f) + 1j * np.sin(f)
+
+        sampled_impedance = zperp(fp)
+
+        # Now create the base matrix
+        submatrix_size = 2 * max_l + 1
+        my_base_matrix = np.zeros((submatrix_size * num_rings,
+                                   submatrix_size * num_rings), dtype=np.complex128)
+
+        for row in range((2*max_l + 1) * num_rings):
+            l = row % submatrix_size - max_l
+            n = row // submatrix_size
+
+            temp = jdict[n][l] * sampled_impedance
+
+            for col in range((2*max_l + 1) * num_rings):
+                lpp = col % submatrix_size - max_l
+                npp = col // submatrix_size
+
+                my_base_matrix[row, col] = 1 / num_rings * (1j)**(l - lpp) * np.sum(temp * jdict[npp][lpp])
+
+        test = vs.NHT(acc1, max_l, num_rings)
+        test.ring_radii = ring_radii
+        base_matrix = test.generateBaseMatrix('perturbed-simple', zperp, max_harmonics, 0)
+
+        self.assertIsNone(np.testing.assert_array_almost_equal(base_matrix, my_base_matrix))
+
+
 if __name__ == '__main__':
     unittest.main()
